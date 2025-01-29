197ec2eb2c044f81615a5f7d67bf062c1b0df538
Anatoliy <ivanoov.anatoly@gmail.com>
2025-01-29 14:40:42 +0500

    add tests

--- src/test/java/DemoqaTest.java
+++ src/test/java/DemoqaTest.java
@@ -5,18 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DemoqaTest {
     @BeforeAll
-    static void DemoqaTest() {
+    static void DemoqaTests() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
     }
         @Test
-        void DemoqaTest1() {
+        void DemoqaTestForm() {
         open("/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
@@ -25,7 +26,27 @@ public class DemoqaTest {
         $("[id=userEmail").setValue("ivanoov.anatoly@gmail.com");
         $("[id=genterWrapper").$(byText("Male")).click();
         $("[id=userNumber]").setValue("89993332222");
-        $("[id=dateOfBirthInput]").setValue("25 Sep 1997");
-
+        $("[id=dateOfBirthInput]").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").selectOption("1997");
+        $(by("aria-label", "Choose Thursday, September 25th, 1997")).click();
+        $("[id=subjectsInput]").setValue("History").pressEnter();
+        $("#uploadPicture").uploadFromClasspath("1.png");
+        $("[id=currentAddress]").setValue("Ufa,Ayskaya 16");
+        $("[id=state]").click();
+        $("[id=react-select-3-input]").setValue("Haryana").pressEnter();
+        $("[id=city").click();
+        $("[id=react-select-4-input]").setValue("Karnal").pressEnter();
+        $("[id=submit]").click();
+        $(".table.table-dark.table-striped.table-bordered.table-hover").shouldHave(text("Ivanov Anatoliy"));
+        $(".table.table-dark.table-striped.table-bordered.table-hover").shouldHave(text("ivanoov.anatoly@gmail.com"));
+        $(".table.table-dark.table-striped.table-bordered.table-hover").shouldHave(text("Male"));
+        $(".table.table-dark.table-striped.table-bordered.table-hover").shouldHave(text("8999333222"));
+        $(".table.table-dark.table-striped.table-bordered.table-hover").shouldHave(text("25 September,1997"));
+        $(".table.table-dark.table-striped.table-bordered.table-hover").shouldHave(text("History"));
+        $(".table.table-dark.table-striped.table-bordered.table-hover").shouldHave(text("Ufa,Ayskaya 16"));
+        $(".table.table-dark.table-striped.table-bordered.table-hover").shouldHave(text("History"));
+        $(".table.table-dark.table-striped.table-bordered.table-hover").shouldHave(text("1.png"));
+        $(".table.table-dark.table-striped.table-bordered.table-hover").shouldHave(text("Haryana Karnal"));
     }
 }
